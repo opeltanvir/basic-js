b6c5f81a42d2958023c2cdd8d9fa21518e30ca61
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2021-08-31 15:31:18 +0600

    upload

--- hello.js
+++ hello.js
@@ -1,27 +1,3 @@
-var  bananapriice = 12;
-console.log(bananapriice);
-var age = 20;
-console.log(typeof age);
-
-var name = "apple"
-console.log(typeof name);
-
-var ishot = true;
-var isRich = false;
-console.log(typeof ishot);
-
-var isrice =true;
-var iswater = false;
-console.log(typeof iswater) 
-
-var name = "apple mahmud"
-console.log(typeof name);
-
-var (1+1=2)=true;
-console.log(typeof 1+1=2);
-
-var name = "hero alom"
-console.log(name);
-
-var promise = "I promise i will work HARD to become a pragrammer";
-console.log(promise.toLowerCase());
+var number1 = 25;
+var number2 = 15.5;
+console.log(number + number2)  
